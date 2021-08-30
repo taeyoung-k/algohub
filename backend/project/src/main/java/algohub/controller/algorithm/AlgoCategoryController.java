47665d0e5de50527ee514feebcc051f7f973aa13
Tae-Young Kim <qordi124@gmail.com>
2021-08-30 21:29:00 +0900

    무중단 서버 배포 테스트

--- backend/project/src/main/java/algohub/controller/algorithm/AlgoCategoryController.java
+++ backend/project/src/main/java/algohub/controller/algorithm/AlgoCategoryController.java
@@ -5,6 +5,8 @@ import algohub.service.algorithm.AlgoCategoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.connector.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
@@ -20,16 +22,16 @@ import java.util.Map;
  * 작성자 : 김태영 (2021-04-18)
  * 내용 : 알고리즘 카테고리
  */
-@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class AlgoCategoryController {
 
     private final AlgoCategoryService categoryService;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("/api/categories")
     public Map<String, Object> category(Model model) {
-        log.info("algorithms categories");
+        logger.info("알고리즘 카테고리 컨트롤러");
 
         List<AlgoCategory> categories = categoryService.getAlgoCategory();
         Map<String, Object> map = new HashMap<>();
